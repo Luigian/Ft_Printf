466a04b46109ab03d8d374a4a0272b7bfd9ebf49
Luis Sanchez <lusanche@e1z3r1p12.42.us.org>
2019-10-25 22:01:16 -0700

    day 01

--- ft_printf.h
+++ ft_printf.h
@@ -6,7 +6,7 @@
 /*   By: lusanche <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/10/16 19:19:00 by lusanche          #+#    #+#             */
-/*   Updated: 2019/10/18 18:50:02 by lusanche         ###   ########.fr       */
+/*   Updated: 2019/10/25 21:58:43 by lusanche         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,27 @@
 #include "libft/libft.h"
 #include <stdarg.h>
 
-int		ft_printf(const char *format, ...);
+typedef struct		s_cs
+{
+	unsigned int	hash;
+	unsigned int	zero;
+	unsigned int	minusSym;
+	unsigned int	space;
+	unsigned int	plusSym;
+	unsigned int	miniWid;
+	unsigned int	precis;
+	unsigned int	lenMod;
+	unsigned int	extPre;
+	unsigned char	typeCon;
+	unsigned char	scapeSec;
+	unsigned char	otherChar;
+}					t_cs;
+
+int		ft_printf(const char *fmt, ...);
 int		ft_print_argument(va_list ap, const char *fmt);
+t_cs	create_cs_object(void);
+int		store_conv_specs(const char *fmt, t_cs conSpec);
+int		is_format_specificator(char c);
+int		is_type_of_conversion(char c);
 
 #endif
